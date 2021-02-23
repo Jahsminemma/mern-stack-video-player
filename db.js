b88ce6ec7486f5105b29ebf46ce4c23a00456335
Okonkwo Emmanuel <jahsminemma@gmail.com>
2021-02-23 06:56:10 +0100

    fix mongoose uri error

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 require("dotenv").config()
 
-mongoose.connect(process.env.DB_CONNECTION,
+mongoose.connect("mongodb://127.0.0.1:27017/tiktok-clone",
     { useNewUrlParser: true, useUnifiedTopology: true }, ()=>console.log("connectd to DB..."))
     
