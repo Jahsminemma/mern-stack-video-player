583c6927b6255516448a35ef855bfa3399857ae1
Okonkwo Emmanuel <jahsminemma@gmail.com>
2021-02-23 07:44:31 +0100

    fix bug

--- db.js
+++ db.js
@@ -4,5 +4,4 @@ require("dotenv").config()
 mongoose.connect("mongodb://127.0.0.1:27017/tiktok-clone",
     { useNewUrlParser: true, useUnifiedTopology: true },
     () => console.log("connected to db"))
-    .catch(err => console.log(err))
     
